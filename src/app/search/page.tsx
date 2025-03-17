8505714e91a0488abb6de3c6368e57ff3a135141
legitminh <totallylegitperson.example@gmail.com>
2025-03-17 16:23:01 -0400

    feat: environmental variable build

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 import Head from "next/head";
 
 interface Agent {
@@ -10,12 +10,23 @@ interface Agent {
 }
 
 export default function Search() {
-  const [agents, setAgents] = useState<Agent[]>([]);
+  const [agents] = useState<Agent[]>([]);
   // const [input, setInput] = useState("");
   const fetchResults = async () => {
     try {
-      fetch("http://localhost:8000/items").then((res) => res.json())
-      .then((data) => setAgents(data));
+      // fetch("http://localhost:8000/items").then((res) => res.json())
+      // .then((data) => setAgents(data));
+      const response = await fetch("https://browser-backend-0fwi.onrender.com/agent/kg/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          agent_name: "string",
+        }),
+      });
+      const data = await response.json();
+      console.log("Response:", data);
     } catch (error) {
       console.error("Error fetching items:", error);
     }
